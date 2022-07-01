980f29941b825884d9e90af325e7c530a2a74537
Enrico Deleo <hello@enricodeleo.com>
2022-07-01 20:50:21 +0200

    fix: code format

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ export default async function (PinoAirbrakeOptions: PinoAirbrakeOptions) {
           },
         });
       } catch (error) {
-        const consoleProperty = console.error ? 'error' : 'log';
+        const consoleProperty = console.error ? "error" : "log";
         console[consoleProperty](error);
       }
     }
